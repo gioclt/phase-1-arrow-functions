cef74cdcd549f10acc9a8646c88f7f54b6f9a5df
Yobany Caltempa <gioclt26@gmail.com>
2022-03-03 11:35:33 -0500

    commit tree 045433802b43eed84cc8e9ea2d91084f30667237

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (num1, num2){
+    console.log (num1/num2)
+        return (20)
+}
+
+divide (2000/100);
+
+const square = (x) => x*x;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
